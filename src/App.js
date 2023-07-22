22af8640e311e4b97115349b59fd02b7e0c3211e
57saurabh <Saurabh.20B0131057@abes.ac.in>
2023-07-22 12:49:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import AllTrains from './components/AllTrains';
+import SingleTrain from './components/SingleTrain';
+import { AppBar, Toolbar, Typography, Container, Button } from '@mui/material';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component={Link} to="/" style={{ textDecoration: 'none', color: 'white', marginRight: '20px' }}>
+            Trian Indian
+          </Typography>
+          <Button component={Link} to="/" color="inherit">
+            All Trains
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg" style={{ marginTop: '20px' }}>
+        <Routes>
+          <Route path="/" element={<AllTrains />} />
+          <Route path="/trains/:trainId" element={<SingleTrain />} />
+        </Routes>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
